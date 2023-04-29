5ba6a67128fe8638dc8867e57c4a5b44da9bc873
joaowesley5678 <joaowesley567@gmail.com>
2023-04-28 21:14:41 -0300

    Alteração do Catalogo de Produtos.

--- Catalogo-de-Produtos/script.js
+++ Catalogo-de-Produtos/script.js
@@ -12,5 +12,5 @@ function carousel() {
     slideIndex = 1;
   }
   x[slideIndex - 1].style.display = "block";
-  setTimeout(carousel, 5000); // Change image every 2 seconds
+  setTimeout(carousel, 5000); // Change image every 5 seconds
 }
